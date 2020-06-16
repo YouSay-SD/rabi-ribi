7b247a3538273a5269b4c147b5410c66c1dc6730
YouSay-SD <yousay.sd@gmail.com>
2020-06-16 18:30:36 -0300

    Optimize Css Animation logoCharacterJump

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,39 +3,23 @@ import styled from 'styled-components';
 
 const HeroStyled = styled.div`
     @keyframes logoSize {
-        0% {
-            transform: scale(1);
-        }
+        0% { transform: scale(1); }
 
-        25% {
-            transform: scale(1.1);
-        }
+        25% { transform: scale(1.1); }
 
-        50% {
-            transform: scale(1);
-        }
+        50% { transform: scale(1); }
 
-        75% {
-            transform: scale(1.05);
-        }
+        75% { transform: scale(1.05); }
 
-        100% {
-            transform: scale(1);
-        }
+        100% { transform: scale(1); }
     }
 
     @keyframes logoCharacterJump {
-        0% {
-            transform: translateY(0);
-        }
+        0% { transform: translateY(0); }
 
-        50% {
-            transform: translateY(-20px);
-        }
+        50% { transform: translateY(-20px); }
 
-        100% {
-            transform: translateY(0);
-        }
+        100% { transform: translateY(0); }
     }
 
     section {
