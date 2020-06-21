60d67538dd67681019edef6e45e5462d5d917f63
YouSay-SD <yousay.sd@gmail.com>
2020-06-21 16:04:59 -0300

    Create Breakpoint Mixin

---
+++ src/theme/theme.js
@@ -0,0 +1,24 @@
+export default {
+    // Colors
+    colors: {
+        primary: '#8A56BB',
+        secondary: '#e85198',
+        terciary: '#262626',
+        black: '#000000',
+        white: '#ffffff',
+        grey: '#f0f0f0'
+    },
+    // Font Family
+    fontFamily: {
+        primary: 'Comic Neue'
+    },
+    // Font Weight 
+    fontWeigth: {
+        light: 300,
+        regular: 400,
+        medium: 500,
+        demi: 600,
+        bold: 700,
+        heavy: 800
+    },
+}
