fc448a8bb087a5f507eb0e424f12308d9c05c942
YouSay-SD <yousay.sd@gmail.com>
2020-06-25 00:00:36 -0300

    Create .rose-background className

--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -13,6 +13,42 @@ const GlobalStyle = createGlobalStyle`
 
     body {
         font-family: 'Comic Neue';
+        background-image: url('./img/background/bg-cross.gif');
+    }
+
+    /* Background Rose */
+    .rose-background {
+        background-image: url('./img/background/bg-pink.png');
+        background-position: center top;
+        padding: 70px 0;
+        margin: 70px 0;
+        position: relative;
+
+        /* Cloud Top */
+        &:before {
+            content: '';
+            background-image: url('./img/background/frame-cloud-top.png');
+            background-position: center bottom;
+            background-repeat: repeat-x;
+            position: absolute;
+            width: 100%;
+            height: 68px;
+            top: -68px;
+            right: 0%;
+        }
+
+        /* Cloud Bottom */
+        &:after {
+            content: '';
+            background-image: url('./img/background/frame-cloud-bottom.png');
+            background-position: center bottom;
+            background-repeat: repeat-x;
+            position: absolute;
+            width: 100%;
+            height: 68px;
+            bottom: -68px;
+            right: 0%;
+        }
     }
 `;
 
