d10c8e6d2d1924204901118082a485363a35d4ff
YouSay-SD <yousay.sd@gmail.com>
2020-06-27 00:08:59 -0300

    Add Styles for Page View Component

--- src/components/pageView.js
+++ src/components/pageView.js
@@ -3,7 +3,13 @@ import styled from 'styled-components';
 import { respondBelow } from '../theme/breakPoints';
 
 const PageViewStyled = styled.article`
-  
+    text-align: center;
+    box-shadow: 0 0 0 2px #ffffff;
+    background-color: ${ props => props.theme.colors.white };
+    border: dotted 1px ${ props => props.theme.colors.secondary };
+    border-radius: 0 0 50px 50px;
+    padding: 5px;
+
     img {
         width: 100%;
         height: 250px;
@@ -20,6 +26,16 @@ const PageViewStyled = styled.article`
             height: 180px;
         `}
     }
+
+    span {
+        width: 100%;
+        font-size: 20px;
+        padding: 7px 0;
+        display: inline-block;
+        color: ${ props => props.theme.colors.secondary };
+        -webkit-text-fill-color: ${ props => props.theme.colors.white };
+        -webkit-text-stroke-width: 1.5px;
+    }
 `;
 
 function PageView({ img, alt, title }) {
@@ -28,6 +44,8 @@ function PageView({ img, alt, title }) {
         <PageViewStyled>
 
             <img src={ img } alt={ alt } title={ title } />
+
+            <span>Page:</span>
 
         </PageViewStyled>
 
