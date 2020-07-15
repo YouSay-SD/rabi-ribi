3240499151e47d42f8cc1338b6c82252d1ef7f56
YouSay-SD <yousay.sd@gmail.com>
2020-07-15 19:55:17 -0300

    Create Item Component

--- src/components/item.js
+++ src/components/item.js
@@ -3,9 +3,45 @@ import styled from "styled-components";
 import { respondBelow } from "../theme/breakPoints";
 
 const ItemStyled = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: ${ props => props.theme.colors.red };
+  width: 200px;
+  height: 30px;
+  border-radius: 30px;
+  cursor: pointer;
+  transition: all .3s ease;
+
   /* Mobile */
   ${respondBelow.sm`
-    `}
+  `}
+
+  &:hover {
+    background-color: ${ props => props.theme.colors.lightRed };
+  }
+`;
+
+const ItemImgStyled = styled.div`
+  width: 40px;
+  border-radius: 30px;
+  margin-right: 10px;
+  background-color: ${ props => props.theme.colors.darkRed };
+  transition: all .3s ease;
+
+  img {
+    width: 100%;
+    transition: all .3s ease;
+  }
+
+  ${ ItemStyled }:hover & {
+    transform: scale(1.1);
+  }
+`;
+
+const ItemTitleStyled = styled.div`
+  span {
+
+  }
 `;
 
 function Item() {
@@ -13,6 +49,17 @@ function Item() {
 
     <ItemStyled>
 
+      <ItemImgStyled>
+
+        <img src="./img/item/fire_orb.png" alt="Fire Orb" />
+
+      </ItemImgStyled>
+
+      <ItemTitleStyled>
+
+        <span>Fire Orb</span>
+
+      </ItemTitleStyled>
 
     </ItemStyled>
 
