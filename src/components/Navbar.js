fbb700645f9b1bf465d18b169c4db867636e9fcc
YouSay-SD <yousay.sd@gmail.com>
2020-08-09 01:56:34 -0300

    WIP - Added New Component Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styled, { keyframes } from 'styled-components';
+import { respondBelow } from '../theme/breakPoints';
+
+const NavStyled = styled.nav`
+    transform: rotate(-1.5deg) translate(35px, 16px);
+    -webkit-transform: rotate(-1.5deg) translate(35px, 16px);
+
+  ul {
+    list-style: none;
+    display: flex;
+
+    li {
+      padding-right: 25px;
+
+      a {
+        color: ${ props => props.theme.colors.secondary };
+        display: inline-block;
+        transition: all .3s ease;
+
+        &:hover {
+          transform: rotate(1deg) scale(1.1);
+          color: ${ props => props.theme.colors.secondary };
+          -webkit-text-fill-color: ${ props => props.theme.colors.white };
+          -webkit-text-stroke-width: 1.5px;
+        }
+      }
+    }
+  }
+
+`;
+
+
+export const Navbar = () => {
+  return (
+    <NavStyled>
+      
+      <ul>
+
+        <li><NavLink exact to="/">Home</NavLink></li>
+        <li><NavLink exact to="/characters">Characters</NavLink></li>
+        <li><NavLink exact to="/game-data">Game Data</NavLink></li>
+        <li><a href="www.google.com">Achievements</a></li>
+        <li><a href="www.google.com">Minigames</a></li>
+        <li><a href="www.google.com">About</a></li>
+
+      </ul>
+
+    </NavStyled>
+  )
+}
